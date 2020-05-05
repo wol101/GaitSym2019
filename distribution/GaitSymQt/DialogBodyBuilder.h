c8d3dc6cf46c174aa2f7d16e7daf451260055661
wol101 <wis@mac.com>
2020-05-05 14:59:32 +0100

    updated for release

--- distribution/GaitSymQt/DialogBodyBuilder.h
+++ distribution/GaitSymQt/DialogBodyBuilder.h
@@ -1,11 +1,16 @@
 #ifndef DIALOGBODYBUILDER_H
 #define DIALOGBODYBUILDER_H
 
+#include "Preferences.h"
+#include "Body.h"
+
 #include <QDialog>
+
+#include <memory>
 
 class QLineEdit;
 class FacetedObject;
-class SimulationWindow;
+class SimulationWidget;
 class Body;
 class LineEditPath;
 class Simulation;
@@ -24,37 +29,39 @@ public:
 
     void lateInitialise();
 
-    void setBody(Body *body);
-    Body *body() const;
-
     Simulation *simulation() const;
     void setSimulation(Simulation *simulation);
 
-    bool createMode() const;
-    void setCreateMode(bool createMode);
+    void setInputBody(Body *inputBody);
+
+    std::unique_ptr<Body> outputBody();
 
 public slots:
     virtual void accept() Q_DECL_OVERRIDE;
     virtual void reject() Q_DECL_OVERRIDE;
+    void properties();
     void calculate();
     void lineEditMeshClicked();
     void lineEditIDTextChanged(const QString &text);
 
+protected:
+    void closeEvent(QCloseEvent *event) Q_DECL_OVERRIDE;
+
 private:
-    void displayMesh(const QString &filename);
     void lineEditMeshActivated(LineEditPath *lineEdit);
 
     Ui::DialogBodyBuilder *ui = nullptr;
 
-    SimulationWindow *m_simulationWindow = nullptr;
     QString m_displayFileName;
 
-    FacetedObject *m_referenceObject = nullptr;
-    Body *m_body = nullptr;
+    std::shared_ptr<FacetedObject> m_referenceObject;
+    Body *m_inputBody = nullptr;
+    std::unique_ptr<Body> m_outputBody;
     Simulation *m_simulation = nullptr;
 
     const std::map<std::string, Body *> *m_existingBodies = nullptr;
-    bool m_createMode = false;
+    QMap<QString, SettingsItem> m_properties;
+
 };
 
 #endif // DIALOGBODYBUILDER_H
