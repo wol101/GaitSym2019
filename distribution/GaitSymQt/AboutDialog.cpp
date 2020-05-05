6a4511e1da3a3603b3470ae486a72b2deb9d247e
wol101 <wis@mac.com>
2020-05-05 14:58:36 +0100

    updated for release

--- distribution/GaitSymQt/AboutDialog.cpp
+++ distribution/GaitSymQt/AboutDialog.cpp
@@ -10,14 +10,54 @@
 #include "AboutDialog.h"
 #include "ui_AboutDialog.h"
 
+#include "Preferences.h"
+
 AboutDialog::AboutDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::AboutDialog)
 {
     ui->setupUi(this);
+#ifdef Q_OS_MACOS
+    setWindowFlags(windowFlags() & (~Qt::Dialog) | Qt::Window); // allows the window to be resized on macs
+#endif
+    restoreGeometry(Preferences::valueQByteArray("AboutDialogGeometry"));
+
+    QString buildDate = QString("Build: %1 %2").arg(__DATE__).arg(__TIME__);
+#ifdef dNODEBUG
+    QString descriptor = QString("Release");
+#else
+    QString descriptor = QString("Debug");
+#endif
+#ifdef EXPERIMENTAL
+    descriptor = QString("%1 %2").arg(descriptor).arg(" Experimental");
+#endif
+
+    ui->labelBuildDate->setText(buildDate);
+    ui->labelDescriptor->setText(descriptor);
 }
 
 AboutDialog::~AboutDialog()
 {
     delete ui;
 }
+
+// note: need to trap closeEvent, accept and reject to be sure of getting all the things that might dismiss the dialog
+
+void AboutDialog::closeEvent(QCloseEvent *event)
+{
+    Preferences::insert("AboutDialogGeometry", saveGeometry());
+    QDialog::closeEvent(event);
+}
+
+void AboutDialog::accept() // this catches cancel, close and escape key
+{
+    Preferences::insert("AboutDialogGeometry", saveGeometry());
+    QDialog::accept();
+}
+
+void AboutDialog::reject() // this catches cancel, close and escape key
+{
+    Preferences::insert("AboutDialogGeometry", saveGeometry());
+    QDialog::reject();
+}
+
