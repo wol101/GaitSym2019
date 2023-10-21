2aaee5193f04c03a4055c0f18ec6760e5068645c
Bill Sellers <wis@mac.com>
2023-10-21 17:25:10 +0100

    More or less complete omniverse kit extension

--- omniverse/kit-exts-usd_batch_render/exts/wis.window.usd_batch_render/wis/window/usd_batch_render/extension.py
+++ omniverse/kit-exts-usd_batch_render/exts/wis.window.usd_batch_render/wis/window/usd_batch_render/extension.py
@@ -1,30 +1,63 @@
 import omni.ui
 import omni.kit
 import carb.events
+import pxr
 
 import os
 import time
 import asyncio
 import pathlib
 import enum
+import json
+import datetime
 
 class action(enum.Enum):
     load = 1
     render = 2
 
 FILETYPES = [".png", ".tga", ".exr"]
+RENDER_PRESETS = ["PATH_TRACE", "RAY_TRACE", "IRAY"]
+
 
 class usd_batch_render(omni.ext.IExt):
-    _viewportWindow = None
-    _viewport = None
-    _output_buffer = ""
+    # I like to define all the class variables
+    # grep -o -e 'self._[A-z_]*' extension.py | sort | uniq
+    _current_path = None
+    _filepicker_selected_folder = None
     _input_folder_picker = None
-    _output_folder_picker = None
-    _usd_file_list_index = 0
-    _usd_file_list = []
     _last_time = 0
     _next_action = action.load
+    _output_buffer = ''
+    _output_folder_picker = None
     _running = False
+    _subs = None
+    _ui_between_file_delay = None
+    _ui_camera_path = None
+    _ui_cancel_button = None
+    _ui_capture_type = None
+    _ui_image_height = None
+    _ui_image_width = None
+    _ui_input_folder_path = None
+    _ui_output_file_name = None
+    _ui_output_folder_path = None
+    _ui_output_text = None
+    _ui_render_preset_index = None
+    _ui_start_button = None
+    _usd_file_list = []
+    _usd_file_list_index = 0
+    _viewport = None
+    _viewportWindow = None
+    _window = None
+    _preferences = {'input_folder':'',
+                    'output_folder':'',
+                    'output_file_name':'',
+                    'file_type_index':0,
+                    'image_width':1920,
+                    'image_height':1080,
+                    'camera_path':'',
+                    'render_preset_index':0,
+                    'between_file_delay':5.0}
+
 
     # Update event.
     def on_update (self, e: carb.events.IEvent):
@@ -36,13 +69,16 @@ class usd_batch_render(omni.ext.IExt):
             self._running is False):
             self._ui_start_button.enabled = True
         else:
-            self._ui_start_button.enabled = False
+            self._ui_start_button.enabled = False            
         
-        if not self._running:
+        if self._running:
+            self._ui_cancel_button.enabled = True
+        else:
+            self._ui_cancel_button.enabled = False
             return
         
         current_time = time.time()
-        if current_time - self._last_time < 5:
+        if current_time - self._last_time < self._preferences['between_file_delay']:
             return
         self._last_time = current_time
 
@@ -59,12 +95,36 @@ class usd_batch_render(omni.ext.IExt):
             
         if self._next_action is action.render:
             capture_extension = omni.kit.capture.viewport.CaptureExtension.get_instance()
-            capture_extension.options._output_folder = self._ui_output_folder_path.model.get_value_as_string()
-            capture_extension.options._file_type = FILETYPES[self._ui_capture_type.model.get_item_value_model().as_int]
-            if self._ui_output_file_name.model.get_value_as_string():
-                capture_extension.options._file_name = self._ui_output_file_name.model.get_value_as_string()
+            capture_extension.options._output_folder = self._preferences['output_folder']
+            capture_extension.options._file_type = FILETYPES[self._preferences['file_type_index']]
+            if self._preferences['output_file_name']:
+                capture_extension.options._file_name = self._preferences['output_file_name']
             else:
                 capture_extension.options._file_name = os.path.split(os.path.splitext(self._usd_file_list[self._usd_file_list_index])[0])[1]
+            capture_extension.options._width = self._preferences['image_width']
+            capture_extension.options._height = self._preferences['image_height']
+            
+            if RENDER_PRESETS[self._preferences['render_preset_index']] == 'PATH_TRACE': capture_extension.options._render_preset = omni.kit.capture.viewport.CaptureRenderPreset.PATH_TRACE
+            if RENDER_PRESETS[self._preferences['render_preset_index']] == 'RAY_TRACE': capture_extension.options._render_preset = omni.kit.capture.viewport.CaptureRenderPreset.RAY_TRACE
+            if RENDER_PRESETS[self._preferences['render_preset_index']] == 'IRAY': capture_extension.options._render_preset = omni.kit.capture.viewport.CaptureRenderPreset.IRAY
+
+            # set the camera
+            stage = self._viewport.usd_context.get_stage()
+            non_omniverse_cameras = []
+            omniverse_cameras = []
+            for prim in pxr.Usd.PrimRange(stage.GetPseudoRoot()):
+                if prim.IsA(pxr.UsdGeom.Camera):
+                    camera_name = prim.GetPath().pathString
+                    if camera_name.startswith('/Omniverse'): omniverse_cameras.append(camera_name)
+                    else: non_omniverse_cameras.append(camera_name)
+            if self._preferences['camera_path'] in non_omniverse_cameras + omniverse_cameras:
+                capture_extension.options._camera = self._preferences['camera_path']
+            else:
+                if non_omniverse_cameras: capture_extension.options._camera = non_omniverse_cameras[0]
+                else: capture_extension.options._camera = omniverse_cameras[0]
+                self._output_buffer += 'Setting camera to "%s"\n' % (capture_extension.options._camera)
+                self._ui_output_text.model.set_value(self._output_buffer)
+
             self._output_buffer += 'Saving "%s%s" to "%s"\n' % (capture_extension.options._file_name, capture_extension.options._file_type, capture_extension.options._output_folder)
             self._ui_output_text.model.set_value(self._output_buffer)
             capture_extension.start()
@@ -76,12 +136,13 @@ class usd_batch_render(omni.ext.IExt):
         self._viewportWindow = omni.ui.Window('Viewport')
         self._viewport = omni.kit.viewport.utility.get_viewport_from_window_name('Viewport')
 
-        self._time = time.time()
-        self._visible = True
         self._current_path = pathlib.Path(__file__).parent.resolve()
 
         # Register for update event.
         self._subs = omni.kit.app.get_app().get_update_event_stream().create_subscription_to_pop(self.on_update)
+
+        # read the preferences
+        self._read_preferences()
 
         # prepare the user interface
         self._window = omni.ui.Window("Batch USD Render",
@@ -92,48 +153,70 @@ class usd_batch_render(omni.ext.IExt):
             with omni.ui.ScrollingFrame():
                 with omni.ui.VStack(spacing=10):
                     omni.ui.Spacer(height=omni.ui.Pixel(1))
-                    with omni.ui.HStack(height=0):
+                    with omni.ui.HStack(height=0, spacing=10):
                         omni.ui.Label("Input Folder:", height=0, width=omni.ui.Pixel(100))
                         self._ui_input_folder_path = omni.ui.StringField(height=0)
+                        self._ui_input_folder_path.model.set_value(self._preferences["input_folder"])
                         omni.ui.Button(omni.ui.get_custom_glyph_code("${glyphs}/folder.svg"),
                                        width=omni.ui.Pixel(60),
                                        mouse_pressed_fn=self._on_input_folder_clicked)
 
-                    with omni.ui.HStack(height=0):
+                    with omni.ui.HStack(height=0, spacing=10):
                         omni.ui.Label("Output Folder:", height=0, width=omni.ui.Pixel(100))
                         self._ui_output_folder_path = omni.ui.StringField(height=0)
+                        self._ui_output_folder_path.model.set_value(self._preferences["output_folder"])
                         omni.ui.Button(omni.ui.get_custom_glyph_code("${glyphs}/folder.svg"), 
                                        width=omni.ui.Pixel(60),
                                        mouse_pressed_fn=self._on_output_folder_clicked)
 
-                    with omni.ui.HStack(height=0):
+                    with omni.ui.HStack(height=0, spacing=10):
                         omni.ui.Label("File Name:", height=0, width=omni.ui.Pixel(100))
                         self._ui_output_file_name = omni.ui.StringField(height=0)
-                        self._ui_output_file_name.model.set_value("Capture")
-                        self._ui_capture_type = omni.ui.ComboBox(0, *FILETYPES, width=omni.ui.Pixel(60))
+                        self._ui_output_file_name.model.set_value(self._preferences["output_file_name"])
+                        self._ui_capture_type = omni.ui.ComboBox(self._preferences["file_type_index"], *FILETYPES, width=omni.ui.Pixel(60))
+                    
+                    omni.ui.Separator(height=0)
+                    with omni.ui.HStack(height=0, spacing=10):
+                        omni.ui.Label("Image Width:", height=0, width=omni.ui.Percent(25))
+                        self._ui_image_width = omni.ui.IntField(height=0, width=omni.ui.Percent(25))
+                        self._ui_image_width.model.set_value(self._preferences["image_width"])
+                        omni.ui.Label("Image Height:", height=0, width=omni.ui.Percent(25))
+                        self._ui_image_height = omni.ui.IntField(height=0, width=omni.ui.Percent(25))
+                        self._ui_image_height.model.set_value(self._preferences["image_height"])
+
+                    with omni.ui.HStack(height=0, spacing=10):
+                        omni.ui.Label("Camera Path:", height=0, width=omni.ui.Percent(25))
+                        self._ui_camera_path = omni.ui.StringField(height=0, width=omni.ui.Percent(25))
+                        self._ui_camera_path.model.set_value(self._preferences["camera_path"])
+                        omni.ui.Label("Loading Delay:", height=0, width=omni.ui.Percent(25))
+                        self._ui_between_file_delay = omni.ui.FloatField(height=0, width=omni.ui.Percent(25))
+                        self._ui_between_file_delay.precision = 1
+                        self._ui_between_file_delay.model.set_value(self._preferences["between_file_delay"])
+
+                    with omni.ui.HStack(height=0, spacing=10):
+                        omni.ui.Label("Render Preset:", height=0, width=omni.ui.Percent(25))
+                        self._ui_render_preset_index = omni.ui.ComboBox(self._preferences["render_preset_index"], *RENDER_PRESETS, width=omni.ui.Percent(25))
 
                     omni.ui.Separator(height=0)
                     with omni.ui.ScrollingFrame():                        
-                        self._ui_output_text = omni.ui.StringField(height=omni.ui.Percent(1000), multiline=True, read_only=True)
-                        self._output_buffer += "USB Batch Render starting\n"
+                        self._ui_output_text = omni.ui.StringField(height=omni.ui.Pixel(10000), width=omni.ui.Pixel(10000), multiline=True, read_only=True)
+                        self._output_buffer += 'USB Batch Render starting at %s in\n"%s"\n' % (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),self._current_path)
                         self._ui_output_text.model.set_value(self._output_buffer)
 
                     omni.ui.Separator(height=0)
                     with omni.ui.HStack(height=0):
                         omni.ui.Spacer()
-                        self._ui_start_button = omni.ui.Button("Start", height=0, width=omni.ui.Pixel(100),
-                                                               mouse_pressed_fn=self._on_start_clicked)
+                        self._ui_cancel_button = omni.ui.Button("Cancel", height=0, width=omni.ui.Pixel(100), mouse_pressed_fn=self._on_cancel_clicked)
+                        self._ui_start_button = omni.ui.Button("Start", height=0, width=omni.ui.Pixel(100), mouse_pressed_fn=self._on_start_clicked)
     
-        self._startup_completed = True
 
 
     def on_shutdown(self):
+        self._write_preferences()
         # Release the update event.
         async def _exitUpdateEvent ():
-            self._visible = False
             await omni.kit.app.get_app().next_update_async()
             self._subs = None
-            self._vTime = None
         asyncio.ensure_future(_exitUpdateEvent())
 
     def _on_input_folder_clicked(self, x, y, b, m):
@@ -195,7 +278,21 @@ class usd_batch_render(omni.ext.IExt):
         self._ui_output_folder_path.model.set_value(self._filepicker_selected_folder)
     
     def _on_start_clicked(self, x, y, b, m):
-        input_folder = self._ui_input_folder_path.model.get_value_as_string()
+        if not self._ui_start_button.enabled:
+            return
+        self._output_buffer += 'Starting\n'
+        self._ui_output_text.model.set_value(self._output_buffer)
+        self._preferences['input_folder'] = self._ui_input_folder_path.model.get_value_as_string()
+        self._preferences['output_folder'] = self._ui_output_folder_path.model.get_value_as_string()
+        self._preferences['output_file_name'] = self._ui_output_file_name.model.get_value_as_string()
+        self._preferences['file_type_index'] = self._ui_capture_type.model.get_item_value_model().as_int
+        self._preferences['image_width'] = self._ui_image_width.model.get_value_as_int()
+        self._preferences['image_height'] = self._ui_image_height.model.get_value_as_int()
+        self._preferences['camera_path'] = self._ui_camera_path.model.get_value_as_string()
+        self._preferences['render_preset_index'] = self._ui_render_preset_index.model.get_item_value_model().as_int
+        self._preferences['between_file_delay'] = self._ui_between_file_delay.model.get_value_as_float()
+        self._write_preferences()
+        input_folder = self._preferences['input_folder']
         extensions = ['.usd' , '.usda' , '.usdc']
         self._next_action = action.load
         self._usd_file_list = []
@@ -211,4 +308,28 @@ class usd_batch_render(omni.ext.IExt):
                 path = os.path.join(input_folder, item)
                 if os.path.isfile(path):
                     self._usd_file_list.append(path)
-        
+
+    def _on_cancel_clicked(self, x, y, b, m):
+        if not self._ui_cancel_button.enabled:
+            return
+        self._output_buffer += 'Cancelling\n'
+        self._ui_output_text.model.set_value(self._output_buffer)
+        self._usd_file_list = []
+        self._usd_file_list_index = 0
+        self._running = False
+
+    def _read_preferences(self):
+        prefs_file_name = os.path.join(self._current_path, "usd_batch_render.json")
+        if os.path.isfile(prefs_file_name):
+            with open(prefs_file_name, 'r', encoding='utf-8') as fp:
+                config_data = json.load(fp)
+                if isinstance(config_data, dict):
+                    for key in self._preferences:
+                        if key in config_data and (isinstance(self._preferences[key], type(config_data[key])) or isinstance(config_data[key], type(self._preferences[key]))):
+                            self._preferences[key] = config_data[key]
+
+    def _write_preferences(self):
+        prefs_file_name = os.path.join(self._current_path, "usd_batch_render.json")
+        with open(prefs_file_name, 'w', encoding='utf-8') as fp:
+            json.dump(self._preferences, fp, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': '))
+
