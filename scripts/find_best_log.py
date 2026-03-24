70ff30a66faf76b6386afb432365762f7f19985b
Bill Sellers <wis@mac.com>
2026-03-24 09:32:46 +0000

    Stripping blank lines

--- scripts/find_best_log.py
+++ scripts/find_best_log.py
@@ -3,16 +3,12 @@
 
 import sys
 import string
-import array
-import os
 import re
-import time
-import math
 import gzip
 
 def find_best_log():
     usageString = "Usage:\n%s logfile1 logfile2 ..." % sys.argv[0]
-    
+
     if len(sys.argv) < 2: PrintExit(usageString)
 
     fileList = sys.argv[1:]
@@ -21,17 +17,18 @@ def find_best_log():
     files = []
     for i in range(0, len(fileList)):
         lines = ConvertFileToLines(fileList[i])
+        lines = [line for line in lines if line.strip()] # get rid of all lines that are whitespace only
         # now need to parse the various options
         lastLine = lines[-1]
         tokens = ConvertToListOfTokens(lastLine)
-        
+
         # check we have a line of numbers
         allNumbers = 1
         for t in tokens:
             if IsANumber(t) == 0:
                 allNumbers = 0
                 break
-        
+
         if allNumbers != 0:
             if (len(tokens) == 5):
                 scores.append(float(tokens[0]))
@@ -39,53 +36,15 @@ def find_best_log():
             if (len(tokens) == 12):
                 scores.append(float(tokens[11]))
                 files.append(fileList[i])
- 
+
     scoresAndFiles = list(zip(scores, files))
     scoresAndFiles.sort()
     #scoresAndFiles.reverse()
     sortedScores, sortedFiles = list(zip(*scoresAndFiles))
-    
+
     for i in range(0, len(sortedScores)):
         print("%7f %s" % (sortedScores[i], sortedFiles[i]))
 
-def find_best_log():
-    usageString = "Usage:\n%s logfile1 logfile2 ..." % sys.argv[0]
-    
-    if len(sys.argv) < 2: PrintExit(usageString)
-
-    fileList = sys.argv[1:]
-
-    scores = []
-    files = []
-    for i in range(0, len(fileList)):
-        lines = ConvertFileToLines(fileList[i])
-        # now need to parse the various options
-        lastLine = lines[-1]
-        tokens = ConvertToListOfTokens(lastLine)
-        
-        # check we have a line of numbers
-        allNumbers = 1
-        for t in tokens:
-            if IsANumber(t) == 0:
-                allNumbers = 0
-                break
-        
-        if allNumbers != 0:
-            if (len(tokens) == 5):
-                scores.append(float(tokens[0]))
-                files.append(fileList[i])
-            if (len(tokens) == 12):
-                scores.append(float(tokens[11]))
-                files.append(fileList[i])
- 
-    scoresAndFiles = list(zip(scores, files))
-    scoresAndFiles.sort()
-    #scoresAndFiles.reverse()
-    sortedScores, sortedFiles = list(zip(*scoresAndFiles))
-    
-    for i in range(0, len(sortedScores)):
-        print("%7f %s" % (sortedScores[i], sortedFiles[i]))
-    
 
 def CopyFile(theInputFileName, theOutputFileName):
     """Copies the contents of a file"""
@@ -116,16 +75,16 @@ def WriteTokenList(filename, tokenList):
     for i in range(0, len(tokenList)):
         theOutput.write(str(tokenList[i]) + "\n")
     theOutput.close()
-    
+
 
 def ConvertToListOfTokens(charList):
     """converts a string into a list of tokens delimited by whitespace"""
-    
+
     i = 0
     tokenList = []
     while i < len(charList):
         byte = charList[i]
-        if byte in string.whitespace: 
+        if byte in string.whitespace:
             i = i + 1
             continue
         word = ""
@@ -143,29 +102,29 @@ def ConvertToListOfTokens(charList):
                 i = i + 1
                 if i >= len(charList): break
                 byte = charList[i]
-        
+
         if len(word) > 0: tokenList.append(word)
         i = i + 1
-            
-    return tokenList        
+
+    return tokenList
 
 def ConvertFileToTokens(filename):
     """reads in a file and converts to a list of whitespace delimited tokens"""
-    
+
     input = open(filename, 'r')
     theBuffer = input.read()
     input.close()
-    
+
     return ConvertToListOfTokens(theBuffer)
-    
+
 def ConvertToListOfTokensIgnoreQuotes(charList):
     """converts a string into a list of tokens delimited by whitespace"""
-    
+
     i = 0
     tokenList = []
     while i < len(charList):
         byte = charList[i]
-        if byte in string.whitespace: 
+        if byte in string.whitespace:
             i = i + 1
             continue
         word = ""
@@ -174,24 +133,24 @@ def ConvertToListOfTokensIgnoreQuotes(charList):
             i = i + 1
             if i >= len(charList): break
             byte = charList[i]
-        
+
         if len(word) > 0: tokenList.append(word)
         i = i + 1
-            
-    return tokenList        
+
+    return tokenList
 
 def ConvertFileToTokensIgnoreQuotes(filename):
     """reads in a file and converts to a list of whitespace delimited tokens"""
-    
+
     input = open(filename, 'r')
     theBuffer = input.read()
     input.close()
-    
-    return ConvertToListOfTokensIgnoreQuotes(theBuffer)  
+
+    return ConvertToListOfTokensIgnoreQuotes(theBuffer)
 
 def ConvertFileToLines(filename):
     """reads in a file and converts to a list of lines"""
-    
+
     if filename.endswith('.gz'):
         input = gzip.open(filename, 'r')
         theLines = input.readlines()
@@ -200,10 +159,10 @@ def ConvertFileToLines(filename):
         input = open(filename, 'r')
         theLines = input.readlines()
         input.close()
-    
-    return theLines 
-    
-    
+
+    return theLines
+
+
 def GetIndexFromTokenList(tokenList, match):
     """returns the index pointing to the value in the tokenList (match + 1)
        returns -1 on error"""
@@ -234,15 +193,13 @@ def GetNumberListFromTokenList(tokenList, match):
             return l
 
     return []
-    
+
 def IsANumber(theString):
     """checks to see whether a string is a valid number"""
-    if re.match(r'([+-]?)(?=\d|\.\d)\d*(\.\d*)?([Ee]([+-]?\d+))?', theString) == None: return 0
+    if re.match(r'^[ \t]*([+-]?)(?=\d|\.\d)\d*(\.\d*)?([Ee]([+-]?\d+))?[ \t]*$', theString) == None: return 0
     return 1
 
 # program starts here
 
 if __name__ == '__main__':
-    find_best_log()   
-   
-
+    find_best_log()
